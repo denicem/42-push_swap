52a74aa99d11ecf5232363949cdb07c136d312f9
Denice Montemayor <dmontema@3-E-4.42heilbronn.de>
2021-11-21 00:21:21 +0100

    add prototype for operation swap.

--- push_swap.c
+++ push_swap.c
@@ -6,13 +6,62 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 19:55:43 by dmontema          #+#    #+#             */
-/*   Updated: 2021/11/21 00:07:47 by dmontema         ###   ########.fr       */
+/*   Updated: 2021/11/21 00:19:20 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/push_swap.h"
 
 #include <stdio.h>
+
+void swap(t_list **stack_a, t_list **stack_b)
+{
+	t_list *first;
+	t_list *sec;
+
+	if (stack_a != NULL && stack_b == NULL)
+	{
+		first = *stack_a;
+		sec = (*stack_a)->next;
+
+		first->next = sec->next;
+		sec->next = first;
+		*stack_a = sec;
+
+		ft_printf("sa\n");
+
+	}
+	if (stack_a == NULL && stack_b != NULL)
+	{
+		first = *stack_b;
+		sec = (*stack_b)->next;
+
+		first->next = sec->next;
+		sec->next = first;
+		*stack_b = sec;
+
+		ft_printf("sb\n");
+
+	}
+	if (stack_a != NULL && stack_b != NULL)
+	{
+		first = *stack_b;
+		sec = (*stack_b)->next;
+
+		first->next = sec->next;
+		sec->next = first;
+		*stack_b = sec;
+
+		first = *stack_b;
+		sec = (*stack_b)->next;
+
+		first->next = sec->next;
+		sec->next = first;
+		*stack_b = sec;
+
+		ft_printf("ss\n");
+	}
+}
 
 void print_lst(void *content)
 {
