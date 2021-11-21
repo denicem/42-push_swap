593e16cf1a32538da51a1edad14e9db740a27b85
Denice Montemayor <dmontema@3-E-4.42heilbronn.de>
2021-11-21 02:25:32 +0100

    add prototype for operation push.

--- push_swap.c
+++ push_swap.c
@@ -6,13 +6,25 @@
 /*   By: dmontema <dmontema@42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/18 19:55:43 by dmontema          #+#    #+#             */
-/*   Updated: 2021/11/21 01:38:53 by dmontema         ###   ########.fr       */
+/*   Updated: 2021/11/21 02:21:57 by dmontema         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "inc/push_swap.h"
 
 #include <stdio.h>
+
+void push (t_list **list_a, t_list **list_b)
+{
+	t_list *tmp;
+
+	tmp = *list_a;
+	*list_a = tmp->next;
+	tmp->next = *list_b;
+	*list_b = tmp;
+
+	ft_printf("p_\n");
+}
 
 void swap(t_list **stack_a, t_list **stack_b)
 {
@@ -63,6 +75,17 @@ void swap(t_list **stack_a, t_list **stack_b)
 	}
 }
 
+void rotate (t_list **list)
+{
+	t_list *tmp = *list;
+
+	*list = tmp->next;
+	tmp->next = NULL;
+	ft_lstadd_back(list, tmp);
+
+	ft_printf("r_\n");
+}
+
 void rev_rotate (t_list **list) //TODO: find another solution, so that you don't have to go through the whole list.
 {
 	t_list *p1;
@@ -95,6 +118,7 @@ int main(int argc, char const *argv[])
 	if (argc > 1)
 	{
 		t_list *list;
+		t_list *list2;
 		int i;
 		int nbr[argc - 1];
 
@@ -105,8 +129,16 @@ int main(int argc, char const *argv[])
 			ft_lstadd_back(&list, ft_lstnew((&nbr[i])));
 			i++;
 		}
-		rev_rotate(&list);
+		// swap(&list, NULL);
+		// rotate(&list);
+		// swap(&list, NULL);
+		// rev_rotate(&list);
+		push(&list, &list2);
+		push(&list, &list2);
+		swap(NULL, &list2);
 		ft_lstiter(list, print_lst);
+		ft_printf("\n");
+		ft_lstiter(list2, print_lst);
 	}
 	else 
 		ft_printf("F*CK\n");
